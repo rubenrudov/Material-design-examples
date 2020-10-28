ec24ba41eb3ca6d255ecf613ab2aad1dab97ee04
rubenrudov <59509727+rubenrudov@users.noreply.github.com>
2020-10-28 19:15:18 +0200

    Add files via upload

--- app/src/main/java/com/materialdesignexamples/MainActivity.java
+++ app/src/main/java/com/materialdesignexamples/MainActivity.java
@@ -1,14 +1,32 @@
 package com.materialdesignexamples;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardView = findViewById(R.id.cardView);
+        cardView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == cardView){
+            startActivity(new Intent(this, HomeActivity.class));
+        }
+        else{
+            //
+        }
     }
 }
